6195f1d72eae95319da8084f2997a555b3c1c390
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2013-10-02 04:48:15 +0000

    * fix intent flags * support opening generic remote host

--- src/com/vuze/android/remote/RemoteUtils.java
+++ src/com/vuze/android/remote/RemoteUtils.java
@@ -1,16 +1,9 @@
 package com.vuze.android.remote;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-import java.util.Map;
-
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Base64;
 
 import com.vuze.android.remote.activity.EmbeddedWebRemote;
-import com.vuze.android.remote.rpc.RPC;
-import com.vuze.android.remote.rpc.RPCException;
 
 public class RemoteUtils
 {
@@ -31,17 +24,39 @@ public class RemoteUtils
 	public void openRemote(final String user, final String ac,
 			final boolean remember, boolean isMain) {
 		System.out.println("openRemote " + ac);
-		
+
 		Intent myIntent = new Intent(activity.getIntent());
 		myIntent.setAction(Intent.ACTION_VIEW);
-		myIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		myIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
 		if (isMain) {
 			myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 		}
 		myIntent.setClassName("com.vuze.android.remote",
 				EmbeddedWebRemote.class.getName());
+		// TODO: put prefs as extra (either as JSON or serializable)
 		myIntent.putExtra("com.vuze.android.remote.ac", ac);
 		myIntent.putExtra("com.vuze.android.remote.user", user);
+		myIntent.putExtra("com.vuze.android.remote.remember", remember);
+		activity.startActivity(myIntent);
+
+	}
+
+	public void openRemote(RemotePreferences prefs, boolean remember,
+			boolean isMain) {
+		Intent myIntent = new Intent(activity.getIntent());
+		myIntent.setAction(Intent.ACTION_VIEW);
+		myIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		if (isMain) {
+			myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+		}
+		myIntent.setClassName("com.vuze.android.remote",
+				EmbeddedWebRemote.class.getName());
+		// TODO: put prefs as extra (either as JSON or serializable)
+		myIntent.putExtra("com.vuze.android.remote.ac", prefs.getAC());
+		myIntent.putExtra("com.vuze.android.remote.user", prefs.getUser());
+		myIntent.putExtra("com.vuze.android.remote.host", prefs.getHost());
+		myIntent.putExtra("com.vuze.android.remote.port", prefs.getPort());
+		myIntent.putExtra("com.vuze.android.remote.nick", prefs.getNick());
 		myIntent.putExtra("com.vuze.android.remote.remember", remember);
 		activity.startActivity(myIntent);
 
