b32762e13821e749905463de60173bd22e64c992
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2013-10-16 07:28:09 +0000

    add version info

--- src/com/vuze/android/remote/dialog/DialogFragmentAbout.java
+++ src/com/vuze/android/remote/dialog/DialogFragmentAbout.java
@@ -3,6 +3,9 @@ package com.vuze.android.remote.dialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.Html;
@@ -28,6 +31,14 @@ public class DialogFragmentAbout
 		tvThanksTo.setMovementMethod(LinkMovementMethod.getInstance());
 		tvThanksTo.setText(Html.fromHtml(getResources().getString(R.string.about_thanks)));
 
+		TextView tvLIcense = (TextView) alertDialogBuilder.view.findViewById(R.id.about_license);
+		try {
+			PackageManager manager = getActivity().getPackageManager();
+			PackageInfo info = manager.getPackageInfo(getActivity().getPackageName(), 0);
+			tvLIcense.setText("Version " + info.versionName + " (build " + info.versionCode + "). GPLv2");
+		} catch (NameNotFoundException e) {
+		}
+		
 		// Add action buttons
 		builder.setPositiveButton(android.R.string.ok,
 				new DialogInterface.OnClickListener() {
