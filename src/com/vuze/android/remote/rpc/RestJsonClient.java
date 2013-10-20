0fa3caa82c359b4060887b486b10a9347cd8314e
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2013-10-20 21:59:05 +0000

    * wrap debug lines * pass stream instead of string to json parser

--- src/com/vuze/android/remote/rpc/RestJsonClient.java
+++ src/com/vuze/android/remote/rpc/RestJsonClient.java
@@ -2,6 +2,7 @@ package com.vuze.android.remote.rpc;
 
 import java.io.*;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -10,15 +11,21 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpProtocolParams;
 import org.json.simple.JSONValue;
+import org.json.simple.parser.ParseException;
+
+import com.vuze.android.remote.AndroidUtils;
 
 public class RestJsonClient
 {
-	public static Object connect(String url) throws RPCException {
-		System.out.println("Execute " + url);
+	public static Object connect(String url)
+			throws RPCException {
+		if (AndroidUtils.DEBUG) {
+			System.out.println("Execute " + url);
+		}
 		long now = System.currentTimeMillis();
 
 		BasicHttpParams basicHttpParams = new BasicHttpParams();
-		HttpProtocolParams.setUserAgent(basicHttpParams, "Tux");
+		HttpProtocolParams.setUserAgent(basicHttpParams, "Vuze Android Remote");
 		HttpClient httpclient = new DefaultHttpClient(basicHttpParams);
 
 		// Prepare a request object
@@ -27,17 +34,15 @@ public class RestJsonClient
 		// Execute the request
 		HttpResponse response;
 
-
 		Object json = null;
-
-		
-		String result = null;
 
 		try {
 			response = httpclient.execute(httpget);
 
 			long then = System.currentTimeMillis();
-			System.out.println("  conn ->" + (then - now) + "ms");
+			if (AndroidUtils.DEBUG) {
+				System.out.println("  conn ->" + (then - now) + "ms");
+			}
 
 			now = then;
 
@@ -47,76 +52,58 @@ public class RestJsonClient
 
 				// A Simple JSON Response Read
 				InputStream instream = entity.getContent();
-				result = convertStreamToString(instream, entity.getContentLength());
+				InputStreamReader isr = new InputStreamReader(instream, "utf8");
+				BufferedReader br = new BufferedReader(isr);
+				br.mark(32767);
 
 				then = System.currentTimeMillis();
-				System.out.println("  readin ->" + (then - now) + "ms");
+				if (AndroidUtils.DEBUG) {
+					System.out.println("  readin ->" + (then - now) + "ms");
+				}
 				now = then;
-				
-				//System.out.println("Resut is " + result);
 
-				if (result.startsWith("(")) {
-					if (result.endsWith(")\n")) {
-						result = result.substring(1, result.length() - 2);
-					} else if (result.endsWith(")"))  {
-						result = result.substring(1, result.length() - 1);
+				try {
+					json = JSONValue.parseWithException(br);
+
+				} catch (ParseException pe) {
+
+					br.reset();
+					String line = br.readLine().trim();
+
+					if (AndroidUtils.DEBUG) {
+						System.out.println("line: " + line);
 					}
-					//System.out.println("result trimmed to " + result);
+					Header contentType = entity.getContentType();
+					if (line.startsWith("<")
+							|| line.contains("<html")
+							|| (contentType != null && contentType.getValue().startsWith(
+									"text/html"))) {
+						// TODO: use android strings.xml
+						throw new RPCException(
+								"Could not retrieve remote client location information.  The most common cause is being on a guest wifi that requires login before using the internet.");
+					}
+
+					throw new RPCException(pe);
+				} finally {
+					br.close();
 				}
-				json = JSONValue.parse(result);
-				
-				System.out.println("JSON Result: " + json);
 
-				instream.close();
-			}
-			
-		} catch (Error e) {
-
-			System.err.println("result = " + result);
-			
-			if (e.getMessage().equals("Lexical Error: Unmatched Input.")) {
-				if (result != null && result.contains("<html")) {
-					// TODO: use android strings.xml
-					throw new RPCException("Could not retrieve remote client location information.  The most common cause is being on a guest wifi that requires login before using the internet.", e);
+				if (AndroidUtils.DEBUG) {
+					System.out.println("JSON Result: " + json);
 				}
-			}
 
-			throw new RPCException(e);
+			}
+		} catch (RPCException e) {
+			throw e;
 		} catch (Throwable e) {
-			System.err.println("result = " + result);
 			throw new RPCException(e);
 		}
 
-		long then = System.currentTimeMillis();
-		System.out.println("  parse ->" + (then - now) + "ms");
-		return json;
-	}
-
-	/**
-	 *
-	 * @param is
-	 * @return String
-	 */
-	public static String convertStreamToString(InputStream is, long len) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is),
-				(int) (len > 4096 ? 4096 : (len < 1024) ? 1024 : len));
-		StringBuilder sb = new StringBuilder();
-
-		String line = null;
-		try {
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				is.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		if (AndroidUtils.DEBUG) {
+			long then = System.currentTimeMillis();
+			System.out.println("  parse ->" + (then - now) + "ms");
 		}
-		return sb.toString();
+		return json;
 	}
 
 }
