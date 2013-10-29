5bc32f3bdcb84f4094fb9220735a99b9d4698c2d
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2013-10-29 00:18:08 +0000

    license

--- src/com/vuze/android/remote/JSInterface.java
+++ src/com/vuze/android/remote/JSInterface.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (C) Azureus Software, Inc, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * 
+ */
+
 package com.vuze.android.remote;
 
 import java.util.ArrayList;
@@ -15,7 +32,6 @@ import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 
 import com.aelitis.azureus.util.JSONUtils;
-import com.google.analytics.tracking.android.EasyTracker;
 import com.google.analytics.tracking.android.MapBuilder;
 import com.vuze.android.remote.activity.LoginActivity;
 import com.vuze.android.remote.activity.MetaSearch;
