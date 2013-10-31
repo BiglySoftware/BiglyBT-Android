1f3a8db671768eddf03520f7e81fce2ddb99a26a
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2013-10-31 23:21:41 +0000

    quotes and remove a few more files

--- build.sh
+++ build.sh
@@ -24,28 +24,32 @@ rm -rf "${DSTDIR}"
 
 echo Copying ${SRCDIR} to ${DSTDIR}
 
-cp -r ${SRCDIR} ${DSTDIR}
-sed -i .bk -e "s/DEBUG = true/DEBUG = false/g" ${DSTDIR}/src/com/vuze/android/remote/AndroidUtils.java 
-rm -r ${DSTDIR}/src/com/vuze/android/remote/AndroidUtils.java.bk
-rm -r ${DSTDIR}/bin/*
-rm -r ${DSTDIR}/gen/*
-find ${DSTDIR} -name '.svn' -exec rm -rf {} \;
-find ${DSTDIR}/assets -name '.[a-z]*' -exec rm -rf {} \;
-find ${DSTDIR}/assets -name 'toolbar-*' -exec rm -rf {} \;
-find ${DSTDIR}/assets -name 'toolbar_butt*' -exec rm -rf {} \;
-find ${DSTDIR}/assets -name '*.scss' -exec rm -rf {} \;
-find ${DSTDIR}/assets -name 'easy*' -exec rm -rf {} \;
-find ${DSTDIR}/src -name '*.txt' -exec rm -rf {} \;
-rm -r ${DSTDIR}/assets/transmission/web/javascript/jquery/jquery-1.10.2.js
-rm -r ${DSTDIR}/assets/transmission/web/javascript/jquery/jquery-ui-1.10.3.custom.js
-rm -r ${DSTDIR}/assets/transmission/web/style/jqueryui/jquery-ui-1.10.3.css
-rm -r ${DSTDIR}/assets/transmission/web/style/transmission/images/buttons/torrent*.png
-rm -r ${DSTDIR}/assets/transmission/web/LICENSE
-rm -rf ${DSTDIR}/assets/transmission/web/images
-rm -rf ${DSTDIR}/assets/transmission/web/style/transmission/images/graphics
+cp -r "${SRCDIR}" "${DSTDIR}"
+sed -i .bk -e "s/DEBUG = true/DEBUG = false/g" "${DSTDIR}/src/com/vuze/android/remote/AndroidUtils.java"
+rm -r "${DSTDIR}/src/com/vuze/android/remote/AndroidUtils.java.bk"
+rm -r "${DSTDIR}/bin/"*
+rm -r "${DSTDIR}/gen/"*
+find "${DSTDIR}" -name '.svn' -exec rm -rf {} \;
+find "${DSTDIR}/assets" -name '.[a-z]*' -exec rm -rf {} \;
+find "${DSTDIR}/assets" -name 'toolbar-*' -exec rm -rf {} \;
+find "${DSTDIR}/assets" -name 'toolbar_butt*' -exec rm -rf {} \;
+find "${DSTDIR}/assets" -name '*.scss' -exec rm -rf {} \;
+find "${DSTDIR}/assets" -name 'easy*' -exec rm -rf {} \;
+find "${DSTDIR}/src" -name '*.txt' -exec rm -rf {} \;
+rm -r "${DSTDIR}/assets/transmission/web/javascript/jquery/jquery-1.10.2.js"
+rm -r "${DSTDIR}/assets/transmission/web/javascript/jquery/jquery-ui-1.10.3.custom.js"
+rm -r "${DSTDIR}/assets/transmission/web/style/jqueryui/jquery-ui-1.10.3.css"
+rm -r "${DSTDIR}/assets/transmission/web/style/transmission/images/buttons/torrent"*.png
+rm -r "${DSTDIR}/assets/transmission/web/LICENSE"
+rm -rf "${DSTDIR}/assets/transmission/web/images"
+rm -rf "${DSTDIR}/assets/transmission/web/style/transmission/images/graphics"
+rm -rf "${DSTDIR}/assets/transmission/web/style/transmission/images/filter_icon.png"
+rm -rf "${DSTDIR}/assets/transmission/web/style/transmission/images/settings.png"
+rm -rf "${DSTDIR}/assets/transmission/web/style/transmission/images/logo.png"
+rm -rf "${DSTDIR}/assets/transmission/web/style/transmission/images/buttons/cancel.png"
 cd "${DSTDIR}"
 android update project --name VuzeAndroidRemote --path .
 ant clean
 ant ${BUILDTYPE}
-cp -f bin/VuzeAndroidRemote-release.apk ../VuzeAndroidRemote-${NEXTVER}.apk
+cp -f "${DSTDIR}/bin/VuzeAndroidRemote-release.apk" ../VuzeAndroidRemote-${NEXTVER}.apk
 tar -czf "${DSTDIR}.tar.gz" .
