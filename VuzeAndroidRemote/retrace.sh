2a86721763e7f8fb8d145462fcb69379b49ed055
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2014-04-08 03:30:18 +0000

    retrace now scans common base class names, handles ":" when "." is meant

--- VuzeAndroidRemote/retrace.sh
+++ VuzeAndroidRemote/retrace.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
+if [ "$1" == "" ] ; then
+	echo $0 '<Release Number> <class>[.method[:line]]'
+	exit
+fi
 androidsdk="`which adb`/../.."
 proguardbin=$androidsdk/tools/proguard/bin
+paths=(""
+	"com.vuze.android.remote."
+	"com.vuze.android.remote.activity."
+	"com.vuze.android.remote.dialog."
+	"com.vuze.android.remote.fragment."
+	"com.vuze.android.remote.rpc."
+	"android.support.v4.app." 
+)
+# Replace all : with .
+class=`echo $2 | sed -e "s/\:\([^0-9]\)/.\1/g"`
+buildno=$1
+
+echo Searching Build $buildno for $class
+
+scan() {
+	gotResult=false
+	for i in "${paths[@]}"
+	do
+	result=`echo ${i}${class} | $proguardbin/retrace.sh -regex "$1" ../VuzeAndroidRemote-${buildno}/VuzeRemoteProject/bin/proguard/mapping.txt`
+	if [ "$result" != "${i}${class}" ] 
+	  then
+	  	if [ "$gotResult" == false ] ; then
+	  		echo
+			echo $2
+			gotResult=true
+		fi
+		echo $result
+	fi
+	done
+}
+
+
+
 echo VuzeAndroidRemote_B$1
-result=`echo $2 | $proguardbin/retrace.sh -regex "%c" ../VuzeAndroidRemote_B$1/bin/proguard/mapping.txt`
-if [ "$result" != "$2" ] 
-  then
-	echo Classes
-	echo $result
-fi
-result=`echo $2 | $proguardbin/retrace.sh -regex "%c\.%m" ../VuzeAndroidRemote_B$1/bin/proguard/mapping.txt`
-if [ "$result" != "$2" ] 
-  then
-	echo Methods
-	echo $result
-fi
-result=`echo $2 | $proguardbin/retrace.sh -regex "%c\.%m:%l" ../VuzeAndroidRemote_B$1/bin/proguard/mapping.txt`
-if [ "$result" != "$2" ] 
-  then
-	echo "Methods (With Lines)"
-	echo $result
-fi
-result=`echo $2 | $proguardbin/retrace.sh -regex "%c\.%f" ../VuzeAndroidRemote_B$1/bin/proguard/mapping.txt`
-if [ "$result" != "$2" ] 
-  then
-	echo Fields
-	echo $result
+
+if [[ $2 =~ .*:[0-9]+$ ]]
+then
+	scan "%c\.%m:%l" "Methods (With Lines)"
+	exit
+else
+	scan "%c" "Classes"
+	scan "%c\.%m" "Methods"
+	scan "%c\.%m:%l" "Methods (With Lines)"
+	scan "%c\.%f" "Fields"
 fi
