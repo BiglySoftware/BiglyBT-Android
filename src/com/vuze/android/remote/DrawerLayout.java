e1b7d3eb5aedf4e0c944e76f968f0780f2c0c24c
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2014-04-10 17:54:40 +0000

    Drawer basics

---
+++ src/com/vuze/android/remote/DrawerLayout.java
@@ -0,0 +1,199 @@
+package com.vuze.android.remote;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityEvent;
+ 
+/**
+ * https://gist.github.com/npombourcq/5636071
+ * Fixes for some issues with the new DrawerLayout.
+ * npombourcq / DrawerLayout.java
+ * 
+ * Fixes focus issues in the base DrawerLayout. This uses code inspired from
+ * ViewPager to ensure that focus goes to the right place depending on drawer
+ * state, namely first to any opened drawer, and if no drawer is opened to the
+ * main content.
+ */
+public final class DrawerLayout extends android.support.v4.widget.DrawerLayout {
+ 
+    private DrawerListener m_wrappedListener;
+ 
+    private final DrawerListener m_drawerListener = new DrawerListener() {
+ 
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            // if the content has focus, transfer focus to the drawer
+            if (getContentView().hasFocus())
+                drawerView.requestFocus(View.FOCUS_FORWARD);
+ 
+            if (m_wrappedListener != null)
+                m_wrappedListener.onDrawerOpened(drawerView);
+        }
+ 
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            if (drawerView.hasFocus())
+                getContentView().requestFocus(View.FOCUS_FORWARD);
+ 
+            if (m_wrappedListener != null)
+                m_wrappedListener.onDrawerClosed(drawerView);
+        }
+ 
+        @Override
+        public void onDrawerSlide(View drawerView, float slideOffset) {
+            if (m_wrappedListener != null)
+                m_wrappedListener.onDrawerSlide(drawerView, slideOffset);
+        }
+ 
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            if (m_wrappedListener != null)
+                m_wrappedListener.onDrawerStateChanged(newState);
+        }
+ 
+    };
+ 
+    private static final OnTouchListener s_drawerTouchListener = new OnTouchListener() {
+ 
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            // prevent touch events from going through the drawer
+            return true;
+        }
+ 
+    };
+ 
+    private final OnHierarchyChangeListener m_hierarchyChangeListener = new OnHierarchyChangeListener() {
+ 
+        @Override
+        public void onChildViewAdded(View parent, View child) {
+            if (DrawerLayout.this == parent && child != getContentView())
+                child.setOnTouchListener(s_drawerTouchListener);
+        }
+ 
+        @Override
+        public void onChildViewRemoved(View parent, View child) {
+ 
+        }
+ 
+    };
+ 
+    public DrawerLayout(Context context) {
+        super(context);
+        init();
+    }
+ 
+    public DrawerLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+ 
+    public DrawerLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+ 
+    private void init() {
+        setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
+        super.setDrawerListener(m_drawerListener);
+        setOnHierarchyChangeListener(m_hierarchyChangeListener);
+    }
+ 
+    private View getContentView() {
+        return getChildAt(0);
+    }
+ 
+    @Override
+    public void setDrawerListener(DrawerListener listener) {
+        m_wrappedListener = listener;
+    }
+ 
+    @Override
+    public void addFocusables(ArrayList<View> views, int direction, int focusableMode) {
+        // logic derived from ViewPager
+ 
+        final int focusableCount = views.size();
+        final int descendantFocusability = getDescendantFocusability();
+ 
+        if (descendantFocusability != FOCUS_BLOCK_DESCENDANTS) {
+            boolean opened = false;
+            for (int i = 1; i < getChildCount(); i++) {
+                final View drawerView = getChildAt(i);
+                if (isDrawerOpen(drawerView)) {
+                    opened = true;
+                    drawerView.addFocusables(views, direction, focusableMode);
+                }
+            }
+ 
+            if (!opened) {
+                getContentView().addFocusables(views, direction, focusableMode);
+            }
+        }
+ 
+        // we add ourselves (if focusable) in all cases except for when we are
+        // FOCUS_AFTER_DESCENDANTS and there are some descendants focusable.
+        // this is
+        // to avoid the focus search finding layouts when a more precise search
+        // among the focusable children would be more interesting.
+        if (descendantFocusability != FOCUS_AFTER_DESCENDANTS ||
+        // No focusable descendants
+                (focusableCount == views.size())) {
+            // Note that we can't call the superclass here, because it will
+            // add all views in. So we need to do the same thing View does.
+            if (!isFocusable()) {
+                return;
+            }
+            if ((focusableMode & FOCUSABLES_TOUCH_MODE) == FOCUSABLES_TOUCH_MODE && isInTouchMode()
+                    && !isFocusableInTouchMode()) {
+                return;
+            }
+            if (views != null) {
+                views.add(this);
+            }
+        }
+    }
+ 
+    @Override
+    protected boolean onRequestFocusInDescendants(int direction, Rect previouslyFocusedRect) {
+        boolean opened = false;
+        for (int i = 1; i < getChildCount(); i++) {
+            final View drawerView = getChildAt(i);
+            if (isDrawerOpen(drawerView)) {
+                opened = true;
+                if (drawerView.requestFocus(direction, previouslyFocusedRect))
+                    return true;
+            }
+        }
+ 
+        if (!opened) {
+            return getContentView().requestFocus(direction, previouslyFocusedRect);
+        }
+ 
+        return false;
+    }
+ 
+    @Override
+    public boolean dispatchPopulateAccessibilityEvent(AccessibilityEvent event) {
+        boolean opened = false;
+        for (int i = 1; i < getChildCount(); i++) {
+            final View drawerView = getChildAt(i);
+            if (isDrawerOpen(drawerView)) {
+                opened = true;
+                if (drawerView.dispatchPopulateAccessibilityEvent(event))
+                    return true;
+            }
+        }
+ 
+        if (!opened)
+            return getContentView().dispatchPopulateAccessibilityEvent(event);
+ 
+        return false;
+    }
+ 
+}
---
+++ src/com/vuze/android/remote/activity/DrawerActivity.java
@@ -0,0 +1,166 @@
+package com.vuze.android.remote.activity;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.vuze.android.remote.R;
+import com.vuze.android.remote.RemoteUtils;
+import com.vuze.android.remote.SessionInfoManager;
+import com.vuze.android.remote.fragment.SessionInfoGetter;
+
+public abstract class DrawerActivity
+	extends ActionBarActivity
+	implements SessionInfoGetter
+{
+
+	private DrawerLayout mDrawerLayout;
+
+	private ActionBarDrawerToggle mDrawerToggle;
+
+	private ListView mDrawerList;
+
+	public void onCreate_setupDrawer() {
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		if (mDrawerLayout == null) {
+			return;
+		}
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close) {
+
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				super.onDrawerClosed(view);
+				DrawerActivity.this.onDrawerClosed(view);
+			}
+
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View view) {
+				super.onDrawerOpened(view);
+				DrawerActivity.this.onDrawerClosed(view);
+			}
+		};
+
+		// Set the drawer toggle as the DrawerListener
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+				R.layout.drawer_list_item, R.id.title, new String[] {
+					"Torrents",
+					"Swarm Discoveries",
+					"Log Out"
+				}));
+		// Set the list's click listener
+		mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				mDrawerLayout.closeDrawer(mDrawerList);
+				switch (position) {
+					case 0: {
+						Intent intent = new Intent(Intent.ACTION_VIEW, null,
+								DrawerActivity.this, TorrentViewActivity.class);
+						intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+						intent.putExtra(SessionInfoManager.BUNDLE_KEY,
+								getSessionInfo().getRemoteProfile().getID());
+						startActivity(intent);
+						break;
+					}
+
+
+					case 1: {
+						Intent intent = new Intent(Intent.ACTION_VIEW, null,
+								DrawerActivity.this, RcmActivity.class);
+						intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+						intent.putExtra(SessionInfoManager.BUNDLE_KEY,
+								getSessionInfo().getRemoteProfile().getID());
+						startActivity(intent);
+						break;
+					}
+
+					case 2: {
+						new RemoteUtils(DrawerActivity.this).openRemoteList(getIntent());
+						SessionInfoManager.removeSessionInfo(getSessionInfo().getRemoteProfile().getID());
+						finish();
+						break;
+					}
+				}
+			}
+		});
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		if (mDrawerToggle != null) {
+			mDrawerToggle.syncState();
+		}
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		if (mDrawerToggle != null) {
+			mDrawerToggle.onConfigurationChanged(newConfig);
+		}
+	}
+
+	public boolean onOptionsItemSelected_drawer(MenuItem item) {
+		if (mDrawerToggle != null && mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return false;
+	}
+
+	public boolean onPrepareOptionsMenu_drawer(Menu menu) {
+
+		if (mDrawerLayout == null) {
+			return true;
+		}
+
+		if (!mDrawerLayout.isDrawerOpen(mDrawerList)) {
+			for (int i = 0; i < menu.size(); i++) {
+				MenuItem item = menu.getItem(i);
+				item.setVisible(true);
+			}
+		} else {
+			final int[] itemsToKeep = {
+				R.id.action_about,
+				R.id.action_logout,
+				R.id.action_social,
+				R.id.action_settings
+			};
+			for (int i = 0; i < menu.size(); i++) {
+				MenuItem item = menu.getItem(i);
+				int id = item.getItemId();
+				boolean visible = false;
+				for (int itemID : itemsToKeep) {
+					if (id == itemID) {
+						visible = true;
+						break;
+					}
+				}
+				item.setVisible(visible);
+			}
+			return false; // skip all other prepares
+		}
+
+		return true;
+	}
+
+	public abstract void onDrawerClosed(View view);
+
+	public abstract void onDrawerOpened(View view);
+}
