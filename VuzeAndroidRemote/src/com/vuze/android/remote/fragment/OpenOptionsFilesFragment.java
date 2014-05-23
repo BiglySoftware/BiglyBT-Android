594bbbfbb651770b9fcab6af8358613837123bea
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2014-05-23 02:07:28 +0000

    Torrent Open Options Dialog

---
+++ VuzeAndroidRemote/src/com/vuze/android/remote/fragment/OpenOptionsFilesFragment.java
@@ -0,0 +1,202 @@
+/**
+ * Copyright (C) Azureus Software, Inc, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.vuze.android.remote.fragment;
+
+import java.util.List;
+import java.util.Map;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.widget.*;
+import android.widget.AbsListView.OnScrollListener;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.vuze.android.remote.*;
+import com.vuze.android.remote.SessionInfo.RpcExecuter;
+import com.vuze.android.remote.activity.TorrentViewActivity;
+import com.vuze.android.remote.rpc.TorrentListReceivedListener;
+import com.vuze.android.remote.rpc.TransmissionRPC;
+
+public class OpenOptionsFilesFragment
+	extends Fragment
+{
+
+	private static final String TAG = "FilesSelection";
+
+	private ListView listview;
+
+	private PullToRefreshListView pullListView;
+
+	private FilesTreeAdapter adapter;
+
+	private SessionInfo sessionInfo;
+
+	private long torrentID;
+
+	private View topView;
+
+	private TextView tvScrollTitle;
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+		if (hasOptionsMenu()) {
+			FragmentActivity activity = getActivity();
+			if (activity instanceof ActionModeBeingReplacedListener) {
+				TorrentViewActivity tva = (TorrentViewActivity) activity;
+				tva.rebuildActionMode();
+			}
+		}
+	}
+
+	public View onCreateView(android.view.LayoutInflater inflater,
+			android.view.ViewGroup container, Bundle savedInstanceState) {
+
+		if (AndroidUtils.DEBUG) {
+			Log.d(TAG, "onCreateview " + this);
+		}
+
+		FragmentActivity activity = getActivity();
+		Intent intent = activity.getIntent();
+
+		final Bundle extras = intent.getExtras();
+		if (extras == null) {
+			Log.e(TAG, "No extras!");
+		} else {
+
+			String remoteProfileID = extras.getString(SessionInfoManager.BUNDLE_KEY);
+			if (remoteProfileID != null) {
+				sessionInfo = SessionInfoManager.getSessionInfo(remoteProfileID,
+						activity);
+			}
+
+			torrentID = extras.getLong("TorrentID");
+		}
+
+		topView = inflater.inflate(R.layout.frag_fileselection, container, false);
+		tvScrollTitle = (TextView) topView.findViewById(R.id.files_scrolltitle);
+
+		View oListView = topView.findViewById(R.id.files_list);
+		if (oListView instanceof ListView) {
+			listview = (ListView) oListView;
+		} else if (oListView instanceof PullToRefreshListView) {
+			pullListView = (PullToRefreshListView) oListView;
+			listview = pullListView.getRefreshableView();
+			pullListView.setMode(Mode.DISABLED);
+		}
+
+		listview.setItemsCanFocus(false);
+		listview.setClickable(true);
+		listview.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+		
+		if (Build.VERSION.SDK_INT >= 19) {
+		  listview.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+		    @SuppressWarnings("deprecation")
+		    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+		    @Override
+		    public void onGlobalLayout() {
+		      listview.setFastScrollEnabled(true);
+		      if (Build.VERSION.SDK_INT >= 16) {
+		        listview.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+		      } else {
+		        listview.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+		      }
+		    }
+		  });
+		} else {
+		  listview.setFastScrollEnabled(true);
+		}
+
+		adapter = new FilesTreeAdapter(this.getActivity());
+		adapter.setInEditMode(true);
+		adapter.setSessionInfo(sessionInfo);
+		listview.setItemsCanFocus(true);
+		listview.setAdapter(adapter);
+
+		listview.setOnScrollListener(new OnScrollListener() {
+			int firstVisibleItem = 0;
+
+			@Override
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+			}
+
+			@Override
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+				if (firstVisibleItem != this.firstVisibleItem) {
+					this.firstVisibleItem = firstVisibleItem;
+					FilesAdapterDisplayObject itemAtPosition = (FilesAdapterDisplayObject) listview.getItemAtPosition(firstVisibleItem);
+
+					if (itemAtPosition == null) {
+						return;
+					}
+					if (itemAtPosition.parent != null) {
+						if (tvScrollTitle != null) {
+							tvScrollTitle.setText(itemAtPosition.parent.folder);
+						}
+					} else {
+						if (tvScrollTitle != null) {
+							tvScrollTitle.setText("");
+						}
+					}
+				}
+			}
+		});
+
+		Map<?, ?> torrent = sessionInfo.getTorrent(torrentID);
+		if (torrent.containsKey("files")) {
+			adapter.setTorrentID(torrentID);
+		} else {
+			sessionInfo.executeRpc(new RpcExecuter() {
+				@Override
+				public void executeRpc(TransmissionRPC rpc) {
+					rpc.getTorrentFileInfo(TAG, torrentID, null,
+							new TorrentListReceivedListener() {
+
+								@Override
+								public void rpcTorrentListReceived(String callID,
+										List<?> addedTorrentMaps, List<?> removedTorrentIDs) {
+									getActivity().runOnUiThread(new Runnable() {
+										@Override
+										public void run() {
+											adapter.setTorrentID(torrentID);
+										}
+									});
+								}
+							});
+				}
+			});
+		}
+
+		if (AndroidUtils.DEBUG) {
+			Log.d(TAG, "set " + adapter + " for " + listview + " to " + sessionInfo
+					+ "/" + torrentID);
+		}
+
+		return topView;
+	}
+
+}
---
+++ VuzeAndroidRemote/src/com/vuze/android/remote/fragment/OpenOptionsGeneralFragment.java
@@ -0,0 +1,224 @@
+/**
+ * Copyright (C) Azureus Software, Inc, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.vuze.android.remote.fragment;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.*;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import com.aelitis.azureus.util.MapUtils;
+import com.vuze.android.remote.*;
+import com.vuze.android.remote.SessionInfo.RpcExecuter;
+import com.vuze.android.remote.activity.TorrentOpenOptionsActivity;
+import com.vuze.android.remote.dialog.DialogFragmentMoveData;
+import com.vuze.android.remote.rpc.TorrentListReceivedListener;
+import com.vuze.android.remote.rpc.TransmissionRPC;
+
+public class OpenOptionsGeneralFragment
+	extends Fragment
+{
+
+	private static final String TAG = "OpenOptionsGeneral";
+
+	private View topView;
+
+	private SessionInfo sessionInfo;
+
+	private long torrentID;
+
+	private TextView tvName;
+
+	private TextView tvSaveLocation;
+
+	private CompoundButton btnPositionLast;
+
+	private CompoundButton btnStateQueued;
+
+	private TorrentOpenOptionsActivity ourActivity;
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)
+	 */
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		if (AndroidUtils.DEBUG) {
+			Log.d(TAG, "onCreateview " + this);
+		}
+
+		FragmentActivity activity = getActivity();
+		Intent intent = activity.getIntent();
+
+		final Bundle extras = intent.getExtras();
+		if (extras == null) {
+			Log.e(TAG, "No extras!");
+		} else {
+
+			String remoteProfileID = extras.getString(SessionInfoManager.BUNDLE_KEY);
+			if (remoteProfileID != null) {
+				sessionInfo = SessionInfoManager.getSessionInfo(remoteProfileID,
+						activity);
+			}
+
+			torrentID = extras.getLong("TorrentID");
+		}
+
+		if (activity instanceof TorrentOpenOptionsActivity) {
+			ourActivity = (TorrentOpenOptionsActivity) activity;
+		}
+
+		topView = inflater.inflate(R.layout.frag_openoptions_general, container,
+				false);
+
+		ImageButton btnEditDir = (ImageButton) topView.findViewById(R.id.openoptions_btn_editdir);
+		ImageButton btnEditName = (ImageButton) topView.findViewById(R.id.openoptions_btn_editname);
+
+		tvName = (TextView) topView.findViewById(R.id.openoptions_name);
+		tvSaveLocation = (TextView) topView.findViewById(R.id.openoptions_saveloc);
+
+		btnPositionLast = (CompoundButton) topView.findViewById(R.id.openoptions_sw_position);
+		btnStateQueued = (CompoundButton) topView.findViewById(R.id.openoptions_sw_state);
+
+		if (ourActivity != null) {
+			if (btnPositionLast != null) {
+				btnPositionLast.setChecked(ourActivity.isPositionLast());
+				btnPositionLast.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+					@Override
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+						ourActivity.setPositionLast(isChecked);
+					}
+				});
+			}
+			if (btnStateQueued != null) {
+				btnStateQueued.setChecked(ourActivity.isStateQueud());
+				btnStateQueued.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+					@Override
+					public void onCheckedChanged(CompoundButton buttonView,
+							boolean isChecked) {
+						ourActivity.setStateQueud(isChecked);
+					}
+				});
+			}
+		}
+
+		final Map<?, ?> torrent = sessionInfo.getTorrent(torrentID);
+
+		if (torrent.containsKey(TransmissionVars.FIELD_TORRENT_DOWNLOAD_DIR)) {
+			updateFields(torrent);
+		} else {
+			sessionInfo.executeRpc(new RpcExecuter() {
+				@Override
+				public void executeRpc(TransmissionRPC rpc) {
+					rpc.getTorrent(TAG, torrentID,
+							Arrays.asList(TransmissionVars.FIELD_TORRENT_DOWNLOAD_DIR),
+							new TorrentListReceivedListener() {
+
+								@Override
+								public void rpcTorrentListReceived(String callID,
+										List<?> addedTorrentMaps, List<?> removedTorrentIDs) {
+									getActivity().runOnUiThread(new Runnable() {
+										@Override
+										public void run() {
+											updateFields(torrent);
+										}
+									});
+								}
+							});
+				}
+			});
+		}
+
+		if (btnEditDir != null) {
+			btnEditDir.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Map<?, ?> torrent = sessionInfo.getTorrent(torrentID);
+					DialogFragmentMoveData.openMoveDataDialog(torrent, sessionInfo,
+							getFragmentManager());
+				}
+			});
+		}
+
+		if (btnEditName != null) {
+			btnEditName.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Builder builder = new AlertDialog.Builder(getActivity());
+					final TextView textView = new EditText(getActivity());
+					textView.setText(tvName.getText());
+					textView.setSingleLine();
+
+					builder.setView(textView);
+					builder.setTitle(R.string.change_name_title);
+					builder.setMessage(R.string.change_name_message);
+					builder.setPositiveButton(android.R.string.ok,
+							new DialogInterface.OnClickListener() {
+
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+									final String newName = textView.getText().toString();
+									tvName.setText(newName);
+									sessionInfo.executeRpc(new RpcExecuter() {
+
+										@Override
+										public void executeRpc(TransmissionRPC rpc) {
+											rpc.setDisplayName(TAG, torrentID, newName);
+										}
+									});
+								}
+							});
+					builder.setNegativeButton(android.R.string.cancel,
+							new DialogInterface.OnClickListener() {
+								@Override
+								public void onClick(DialogInterface dialog, int which) {
+								}
+							});
+					builder.create().show();
+				}
+			});
+		}
+
+		return topView;
+	}
+
+	private void updateFields(Map<?, ?> torrent) {
+		if (tvName != null) {
+			tvName.setText(MapUtils.getMapString(torrent, "name", "dunno"));
+		}
+		if (tvSaveLocation != null) {
+			String saveLocation = TorrentUtils.getSaveLocation(torrent);
+			tvSaveLocation.setText(saveLocation);
+		}
+
+	}
+
+}
