0727950844f13e14c3a2dbb7d7b5f8b5c3dadc0d
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2014-05-23 02:07:28 +0000

    Torrent Open Options Dialog

---
+++ src/com/vuze/android/remote/activity/TorrentOpenOptionsActivity.java
@@ -0,0 +1,243 @@
+/**
+ * Copyright (C) Azureus Software, Inc, All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+package com.vuze.android.remote.activity;
+
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.*;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+
+import com.astuetz.PagerSlidingTabStrip;
+import com.vuze.android.remote.*;
+import com.vuze.android.remote.SessionInfo.RpcExecuter;
+import com.vuze.android.remote.fragment.OpenOptionsPagerAdapter;
+import com.vuze.android.remote.rpc.TransmissionRPC;
+
+/**
+ * Open Torrent: Options Dialog (Window)
+ * <p>
+ * Many Layouts!
+ * <p>
+ * 1) Wide and Long:  General Info on the left, File List on the right
+ * 2) Just Long: General Info on top, File List on bottom
+ * 3) Small: Tabs with General and Files
+ */
+public class TorrentOpenOptionsActivity
+	extends ActionBarActivity
+{
+	private static final String TAG = "TorrentOpenOptions";
+
+	private SessionInfo sessionInfo;
+
+
+	private long torrentID;
+
+	private OpenOptionsPagerAdapter pagerAdapter;
+
+	protected boolean positionLast = true;
+
+	protected boolean stateQueud = true;
+
+	/* (non-Javadoc)
+	* @see android.support.v4.app.FragmentActivity#onCreate(android.os.Bundle)
+	*/
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Intent intent = getIntent();
+
+		final Bundle extras = intent.getExtras();
+		if (extras == null) {
+			Log.e(TAG, "No extras!");
+			finish();
+			return;
+		}
+
+		String remoteProfileID = extras.getString(SessionInfoManager.BUNDLE_KEY);
+		if (remoteProfileID != null) {
+			sessionInfo = SessionInfoManager.getSessionInfo(remoteProfileID, this);
+		}
+
+		torrentID = extras.getLong("TorrentID");
+
+		if (sessionInfo == null) {
+			Log.e(TAG, "sessionInfo NULL!");
+			finish();
+			return;
+		}
+
+		setupActionBar();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+			setupIceCream();
+		}
+
+		setContentView(R.layout.activity_torrent_openoptions);
+
+		ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+		PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) findViewById(R.id.pager_title_strip);
+		if (viewPager != null && tabs != null) {
+			pagerAdapter = new OpenOptionsPagerAdapter(
+					getSupportFragmentManager(), viewPager, tabs);
+		}
+
+		Button btnAdd = (Button) findViewById(R.id.openoptions_btn_add);
+		Button btnCancel = (Button) findViewById(R.id.openoptions_btn_cancel);
+		CompoundButton cbSilentAdd = (CompoundButton) findViewById(R.id.openoptions_cb_silentadd);
+
+		if (btnAdd != null) {
+			btnAdd.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					finish(true);
+				}
+			});
+		}
+		if (btnCancel != null) {
+			btnCancel.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					finish(false);
+				}
+			});
+		}
+		if (cbSilentAdd != null) {
+			cbSilentAdd.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+				@Override
+				public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+					sessionInfo.getRemoteProfile().setAddTorrentSilently(isChecked);
+				}
+			});
+			cbSilentAdd.setChecked(sessionInfo.getRemoteProfile().isAddTorrentSilently());
+		}
+
+	}
+
+	protected void finish(boolean addTorrent) {
+		if (addTorrent) {
+			// set position and state, the rest are already set
+			sessionInfo.executeRpc(new RpcExecuter() {
+				@Override
+				public void executeRpc(TransmissionRPC rpc) {
+					rpc.simpleRpcCall(positionLast  ? "queue-move-bottom"
+							: "queue-move-top", new long[] {
+						torrentID
+					}, null);
+					if (stateQueud) {
+						rpc.startTorrents("OpenOptions", new long[] {
+							torrentID
+						}, false, null);
+					} else {
+						// should be already stopped, but stop anyway
+						rpc.stopTorrents("OpenOptions", new long[] {
+							torrentID
+						}, null);
+					}
+				}
+			});
+		} else {
+			// remove the torrent
+			sessionInfo.executeRpc(new RpcExecuter() {
+				@Override
+				public void executeRpc(TransmissionRPC rpc) {
+					rpc.removeTorrent(new long[] {
+						torrentID
+					}, true, null);
+				}
+			});
+		}
+		if (!isFinishing()) {
+			finish();
+		}
+	}
+
+	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	private void setupIceCream() {
+		getActionBar().setHomeButtonEnabled(true);
+	}
+
+	private void setupActionBar() {
+		ActionBar actionBar = getSupportActionBar();
+		if (actionBar == null) {
+			if (AndroidUtils.DEBUG) {
+				System.err.println("actionBar is null");
+			}
+			return;
+		}
+		actionBar.setDisplayHomeAsUpEnabled(false);
+
+		RemoteProfile remoteProfile = sessionInfo.getRemoteProfile();
+		actionBar.setSubtitle(remoteProfile.getNick());
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onPause()
+	 */
+	@Override
+	protected void onPause() {
+		if (pagerAdapter != null) {
+			pagerAdapter.onPause();
+		}
+
+		super.onPause();
+	}
+
+	/* (non-Javadoc)
+	 * @see android.support.v4.app.FragmentActivity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+
+		if (pagerAdapter != null) {
+			pagerAdapter.onResume();
+		}
+	}
+	
+	/* (non-Javadoc)
+	 * @see android.support.v7.app.ActionBarActivity#onBackPressed()
+	 */
+	@Override
+	public void onBackPressed() {
+		finish(false);
+		super.onBackPressed();
+	}
+
+	public boolean isPositionLast() {
+		return positionLast;
+	}
+
+	public boolean isStateQueud() {
+		return stateQueud;
+	}
+
+	public void setPositionLast(boolean positionLast) {
+		this.positionLast = positionLast;
+	}
+
+	public void setStateQueud(boolean stateQueud) {
+		this.stateQueud = stateQueud;
+	}
+}
