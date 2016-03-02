be480271c6414399d8e2bc8b9e7d0a07b267d482
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2016-03-02 19:46:58 +0000

    Menu item refreshing, and refresh button animation

--- vuzeAndroidRemote/src/main/java/com/vuze/android/remote/fragment/SideActionsAdapter.java
+++ vuzeAndroidRemote/src/main/java/com/vuze/android/remote/fragment/SideActionsAdapter.java
@@ -22,7 +22,10 @@ import java.util.List;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v7.view.menu.MenuBuilder;
+import android.support.v7.widget.RecyclerView;
 import android.view.*;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,6 +34,8 @@ import com.vuze.android.FlexibleRecyclerSelectionListener;
 import com.vuze.android.FlexibleRecyclerViewHolder;
 import com.vuze.android.remote.AndroidUtilsUI;
 import com.vuze.android.remote.R;
+import com.vuze.android.remote.SessionInfo;
+import com.vuze.android.remote.activity.TorrentViewActivity;
 
 /**
  * Created by TuxPaper on 2/13/16.
@@ -39,10 +44,24 @@ public class SideActionsAdapter
 	extends
 	FlexibleRecyclerAdapter<SideActionsAdapter.SideActionsHolder, SideActionsAdapter.SideActionsInfo>
 {
-
 	private static final String TAG = "SideActionsAdapter";
 
+	private static final int menu_ids[] = new int[] {
+		R.id.action_refresh,
+		R.id.action_add_torrent,
+		R.id.action_search,
+		R.id.action_start_all,
+		R.id.action_stop_all,
+		R.id.action_settings,
+		R.id.action_social,
+		R.id.action_logout
+	};
+
 	private final Context context;
+
+	private final SessionInfo sessionInfo;
+
+	private final MenuBuilder menuBuilder;
 
 	public static final class SideActionsInfo
 	{
@@ -63,6 +82,8 @@ public class SideActionsAdapter
 
 		final ImageView iv;
 
+		public RotateAnimation rotateAnimation;
+
 		public SideActionsHolder(RecyclerSelectorInternal selector, View rowView) {
 			super(selector, rowView);
 
@@ -73,36 +94,48 @@ public class SideActionsAdapter
 		}
 	}
 
-	public SideActionsAdapter(Context context,
+	public SideActionsAdapter(Context context, SessionInfo sessionInfo,
 			FlexibleRecyclerSelectionListener selector) {
 		super(selector);
 		this.context = context;
+		this.sessionInfo = sessionInfo;
 		setHasStableIds(true);
 
-		List<SideActionsInfo> list = new ArrayList<>();
-
-		MenuBuilder menuBuilder = new MenuBuilder(context);
+		menuBuilder = new MenuBuilder(context);
 		new MenuInflater(context).inflate(R.menu.menu_torrent_list, menuBuilder);
 
-		int[] ids = {
-			R.id.action_refresh,
-			R.id.action_add_torrent,
-			R.id.action_search,
-			R.id.action_start_all,
-			R.id.action_stop_all,
-			R.id.action_settings,
-			R.id.action_social,
-			R.id.action_logout,
-		};
+		updateMenuItems();
+	}
 
-		for (int id : ids) {
+	public void updateMenuItems() {
+
+		TorrentViewActivity.prepareGlobalMenu(menuBuilder, sessionInfo);
+
+		List<SideActionsInfo> list = new ArrayList<>();
+		for (int id : menu_ids) {
 			MenuItem item = menuBuilder.findItem(id);
-			if (item != null) {
+			if (item != null && item.isVisible()) {
 				list.add(new SideActionsInfo(item));
 			}
 		}
 
 		setItems(list);
+	}
+
+	public void updateRefreshButton() {
+		RecyclerView.ViewHolder vh = getRecyclerView().findViewHolderForItemId(
+				R.id.action_refresh);
+		if (vh != null) {
+
+			int position = vh.getAdapterPosition();
+			if (position >= 0) {
+				SideActionsInfo item = getItem(position);
+				if (item != null) {
+					item.menuItem.setEnabled(!sessionInfo.isRefreshingTorrentList());
+					notifyItemChanged(position);
+				}
+			}
+		}
 	}
 
 	@Override
@@ -113,9 +146,7 @@ public class SideActionsAdapter
 
 		View rowView = inflater.inflate(R.layout.row_sideaction, parent, false);
 
-		SideActionsHolder vh = new SideActionsHolder(this, rowView);
-
-		return vh;
+		return new SideActionsHolder(this, rowView);
 	}
 
 	@Override
@@ -130,6 +161,26 @@ public class SideActionsAdapter
 		holder.tvText.setVisibility(isSmall ? View.GONE : View.VISIBLE);
 		Drawable icon = item.menuItem.getIcon();
 		holder.iv.setImageDrawable(icon);
+
+		if (item.menuItem.getItemId() == R.id.action_refresh) {
+			if (sessionInfo.isRefreshingTorrentList()) {
+				if (holder.rotateAnimation == null) {
+					holder.rotateAnimation = new RotateAnimation(0, 360,
+							RotateAnimation.RELATIVE_TO_SELF, 0.5f,
+							RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+					holder.rotateAnimation.setDuration(1000);
+					holder.rotateAnimation.setInterpolator(new LinearInterpolator());
+					holder.rotateAnimation.setRepeatCount(RotateAnimation.INFINITE);
+					holder.iv.startAnimation(holder.rotateAnimation);
+				}
+			} else {
+				holder.iv.clearAnimation();
+				holder.rotateAnimation = null;
+			}
+		}
+
+		holder.itemView.setEnabled(item.menuItem.isEnabled());
+		holder.iv.setAlpha(item.menuItem.isEnabled() ? 0xFF : 0x80);
 	}
 
 	@Override
