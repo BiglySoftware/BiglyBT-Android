7b70bce13ef0bb9ff10cf4b6f02b40dbbc88ef53
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2016-03-07 20:58:57 +0000

    New GA only API >= 9

--- vuzeAndroidRemote/src/main/java/com/vuze/android/remote/VuzeEasyTracker.java
+++ vuzeAndroidRemote/src/main/java/com/vuze/android/remote/VuzeEasyTracker.java
@@ -17,9 +17,13 @@
 
 package com.vuze.android.remote;
 
+import android.app.Activity;
 import android.content.Context;
+import android.os.Build;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+
+import java.util.Map;
 
 public class VuzeEasyTracker
 {
@@ -28,10 +32,91 @@ public class VuzeEasyTracker
 	public static IVuzeEasyTracker getInstance(Context ctx) {
 		synchronized (VuzeEasyTracker.class) {
 			if (vuzeEasyTracker == null) {
-				if (AndroidUtils.DEBUG) {
-					Log.d("VET", "Using old GA API");
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+					vuzeEasyTracker = new VuzeEasyTrackerNew(ctx);
+				} else {
+					if (AndroidUtils.DEBUG) {
+						Log.d("VET", "Using old GA API");
+					}
+					//vuzeEasyTracker = new VuzeEasyTrackerOld(ctx);
+					vuzeEasyTracker = new IVuzeEasyTracker() {
+						@Override
+						public void activityStart(Activity activity) {
+
+						}
+
+						@Override
+						public void screenStart(String name) {
+
+						}
+
+						@Override
+						public void fragmentStart(Fragment fragment, String name) {
+
+						}
+
+						@Override
+						public void activityStop(Activity activity) {
+
+						}
+
+						@Override
+						public void fragmentStop(Fragment fragment) {
+
+						}
+
+						@Override
+						public String get(String key) {
+							return null;
+						}
+
+						@Override
+						public void send(Map<String, String> params) {
+
+						}
+
+						@Override
+						public void set(String key, String value) {
+
+						}
+
+						@Override
+						public void logError(String s, String page) {
+
+						}
+
+						@Override
+						public void logError(Throwable e) {
+
+						}
+
+						@Override
+						public void logErrorNoLines(Throwable e) {
+
+						}
+
+						@Override
+						public void sendEvent(String category, String action, String label,
+								Long value) {
+
+						}
+
+						@Override
+						public void registerExceptionReporter(Context applicationContext) {
+
+						}
+
+						@Override
+						public void setClientID(String rt) {
+
+						}
+
+						@Override
+						public void setPage(String rt) {
+
+						}
+					};
 				}
-				vuzeEasyTracker = new VuzeEasyTrackerOld(ctx);
 			}
 		}
 		return vuzeEasyTracker;
