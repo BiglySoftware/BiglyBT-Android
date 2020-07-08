72675d38d380dc2e952571e90248eea0d1dbfa92
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2020-07-07 22:44:20 -0700

    AssumeNoSideEffects annotation and ProGuard/R8 rules to implement them

---
+++ BiglyBT/src/main/java/com/biglybt/util/AssumeNoSideEffects.java
@@ -0,0 +1,44 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.biglybt.util;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Denotes that the annotated element has no side effect, and can be removed.
+ * See Proguard/R8 documentation.
+ * <p>
+ * Example:
+ * <pre><code>
+ *  &#64;AssumeNoSideEffects
+ *  public void foo() {
+ *      ...
+ *  }
+ * </code></pre>
+ */
+@Retention(CLASS)
+@Target({CONSTRUCTOR,METHOD,FIELD})
+public @interface AssumeNoSideEffects {
+}
