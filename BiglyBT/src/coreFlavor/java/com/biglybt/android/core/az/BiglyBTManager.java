6f7c8d26d99bbc0153b6eb312244a84064e5f04b
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2020-09-03 23:56:28 -0700

    no IgnoreFiles by default (slows down torrent move)

--- BiglyBT/src/coreFlavor/java/com/biglybt/android/core/az/BiglyBTManager.java
+++ BiglyBT/src/coreFlavor/java/com/biglybt/android/core/az/BiglyBTManager.java
@@ -296,6 +296,9 @@ public class BiglyBTManager
 		coreDefaults.addParameter("Tracker Client Closedown Timeout", 5);
 		coreDefaults.addParameter("Tracker Client Numwant Limit", 10);
 
+		// Having IgnoreFiles slows down torrent move
+		coreDefaults.addParameter("File.Torrent.IgnoreFiles", "");
+
 		PluginManagerDefaults defaults = PluginManager.getDefaults();
 
 		defaults.setDefaultPluginEnabled(PluginManagerDefaults.PID_BUDDY, false);
