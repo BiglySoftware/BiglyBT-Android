f4eb3c42313b3c977b7ddb8317e2705ae29c4c65
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2021-02-08 14:11:59 -0800

    javadoc

--- app/src/main/java/com/biglybt/android/client/fragment/SessionFragment.java
+++ app/src/main/java/com/biglybt/android/client/fragment/SessionFragment.java
@@ -111,6 +111,34 @@ public abstract class SessionFragment
 		return onCreateViewWithSession(inflater, container, savedInstanceState);
 	}
 
+	/**
+	 * <i>From {@link androidx.fragment.app.Fragment#onCreateView(android.view.LayoutInflater, android.view.ViewGroup, android.os.Bundle)}:</i>
+	 * <p/>
+	 * Called to have the fragment instantiate its user interface view.
+	 * This is optional, and non-graphical fragments can return null. This will be called between
+	 * {@link #onCreate(Bundle)} and {@link #onActivityCreated(Bundle)}.
+	 * <p>A default View can be returned by calling {@link #Fragment(int)} in your
+	 * constructor. Otherwise, this method returns null.
+	 *
+	 * <p>It is recommended to <strong>only</strong> inflate the layout in this method and move
+	 * logic that operates on the returned View to {@link #onViewCreated(View, Bundle)}.
+	 *
+	 * <p>If you return a View from here, you will later be called in
+	 * {@link #onDestroyView} when the view is being released.
+	 *
+	 * @param inflater The LayoutInflater object that can be used to inflate
+	 * any views in the fragment,
+	 * @param container If non-null, this is the parent view that the fragment's
+	 * UI should be attached to.  The fragment should not add the view itself,
+	 * but this can be used to generate the LayoutParams of the view.
+	 * @param savedInstanceState If non-null, this fragment is being re-constructed
+	 * from a previous saved state as given here.
+	 * 
+	 * @apiNote {@link android.app.Activity#findViewById(int)} will not work here.
+	 * Use {@link #onActivityCreated(Bundle)} instead.
+	 *
+	 * @return Return the View for the fragment's UI, or null.
+	 */
 	@SuppressWarnings("WeakerAccess")
 	@UiThread
 	public abstract View onCreateViewWithSession(@NonNull LayoutInflater inflater,
