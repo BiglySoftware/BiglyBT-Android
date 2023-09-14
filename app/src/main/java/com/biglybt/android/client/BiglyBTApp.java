e545bd8de9285ea34203cece3f6c22d8a3dc319a
TuxPaper <725353+TuxPaper@users.noreply.github.com>
2023-09-14 14:18:37 -0700

    revert refresh on getNetworkState() as it's called frequently

--- app/src/main/java/com/biglybt/android/client/BiglyBTApp.java
+++ app/src/main/java/com/biglybt/android/client/BiglyBTApp.java
@@ -454,10 +454,6 @@ public class BiglyBTApp
 	public static NetworkState getNetworkState() {
 		if (networkState == null) {
 			networkState = new NetworkState(applicationContext);
-		} else {
-			// saw some weird edge case where app may get out of sync with state
-			// force refresh
-			networkState.refresh();
 		}
 		return networkState;
 	}
